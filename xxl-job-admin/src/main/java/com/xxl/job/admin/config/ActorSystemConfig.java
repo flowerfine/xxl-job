e77f659610cb0c75495ec1e7bc14c6547781fb60
wangqi <1942460489@qq.com>
2021-12-19 23:20:13 +0800

    feature: executor registry.

--- xxl-job-admin/src/main/java/com/xxl/job/admin/config/ActorSystemConfig.java
+++ xxl-job-admin/src/main/java/com/xxl/job/admin/config/ActorSystemConfig.java
@@ -5,20 +5,16 @@ import akka.actor.typed.SpawnProtocol;
 import akka.actor.typed.javadsl.Behaviors;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ActorSystemConfig {
 
-    @Value("${spring.application.name:xxl-job-admin}")
-    private String application;
-
     @Bean(destroyMethod = "terminate")
     public ActorSystem<SpawnProtocol.Command> actorSystem() {
         Config config = ConfigFactory.load("xxl-job.conf");
-        ActorSystem<SpawnProtocol.Command> actorSystem = ActorSystem.create(Behaviors.setup(ctx -> SpawnProtocol.create()), application, config);
+        ActorSystem<SpawnProtocol.Command> actorSystem = ActorSystem.create(Behaviors.setup(ctx -> SpawnProtocol.create()), "xxl-job", config);
         actorSystem.whenTerminated().onComplete(done -> {
             if (done.isSuccess()) {
                 actorSystem.log().info("ActorSystem terminate success!");
