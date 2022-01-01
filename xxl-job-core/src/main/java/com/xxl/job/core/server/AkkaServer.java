bfedd50282d3ce23958e8ce8c8b9d87d220b7849
wangqi <1942460489@qq.com>
2022-01-02 00:10:05 +0800

    feature: rpc

--- xxl-job-core/src/main/java/com/xxl/job/core/server/AkkaServer.java
+++ xxl-job-core/src/main/java/com/xxl/job/core/server/AkkaServer.java
@@ -5,8 +5,11 @@ import akka.actor.typed.SpawnProtocol;
 import com.xxl.job.core.remote.ExecutorServiceImpl;
 import com.xxl.job.rpc.ActorSelectionHelper;
 import com.xxl.job.rpc.AkkaRpcService;
+import com.xxl.job.rpc.RpcServer;
 import com.xxl.job.rpc.RpcService;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CompletableFuture;
 
 @Slf4j
 public class AkkaServer {
@@ -21,6 +24,13 @@ public class AkkaServer {
     }
 
     private void startExecutor() {
-        rpcService.start(ActorSelectionHelper.EXECUTOR_ENDPOINT, new ExecutorServiceImpl());
+        CompletableFuture<RpcServer> future = rpcService.start(ActorSelectionHelper.EXECUTOR_ENDPOINT, new ExecutorServiceImpl());
+        future.whenComplete((server, throwable) -> {
+            if (throwable != null) {
+                log.error("启动 akka server 异常！", throwable);
+            } else {
+                log.info("启动 akka server 成功! path: {}", server.getActorRef().path().address());
+            }
+        });
     }
 }
